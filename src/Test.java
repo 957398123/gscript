cbfeedd657e9810474e65585e538750e983fbd6e
957398123 <957398123@qq.com>
2025-06-26 11:49:20 +0800

    新增：新增测试脚本。

--- src/Test.java
+++ src/Test.java
@@ -15,30 +15,37 @@ import java.util.List;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-        URL classUrl = Test.class.getResource("Test.class");
-        // 提取 Test.class 的所在目录路径
-        File classFile = new File(classUrl.getPath());
-        String classDir = classFile.getParent();
-        String fileName = "test";
-        InputStream in = Test.class.getResourceAsStream(fileName + ".script");
-        if (in != null) {
-            String content = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-            in.close();
-            Lexer lexer = new Lexer();
-            List<GSToken> tokens = lexer.tokenize(content);
-            Parser parser = new Parser(tokens);
-            Node node = parser.parseProgram();
-            ByteCodeGenerator bytecodeGenerator = new ByteCodeGenerator();
-            node.accept(bytecodeGenerator);
-            bytecodeGenerator.print();
-            // 解释执行
-            // System.out.println("GS Engine Start!");
-            ByteCodeSerialize.serialize(bytecodeGenerator.getByteCode(), classDir + File.separator + fileName + ".gclass");
-            if ("test".equals(fileName)) {
-                Interpreter interpreter = new Interpreter();
-                interpreter.eval(bytecodeGenerator.getByteCode());
+        String[] list = new String[]{"cons", "tj", "util", "member", "battle", "autoplay"};
+        Test.gen(list);
+    }
+
+    public static void gen(String[] files) throws IOException {
+        for (int i = 0; i < files.length; i++) {
+            URL classUrl = Test.class.getResource("Test.class");
+            // 提取 Test.class 的所在目录路径
+            File classFile = new File(classUrl.getPath());
+            String classDir = classFile.getParent();
+            String fileName = files[i];
+            InputStream in = Test.class.getResourceAsStream(fileName + ".script");
+            if (in != null) {
+                String content = new String(in.readAllBytes(), StandardCharsets.UTF_8);
+                in.close();
+                Lexer lexer = new Lexer();
+                List<GSToken> tokens = lexer.tokenize(content);
+                Parser parser = new Parser(tokens);
+                Node node = parser.parseProgram();
+                ByteCodeGenerator bytecodeGenerator = new ByteCodeGenerator();
+                node.accept(bytecodeGenerator);
+                // bytecodeGenerator.print();
+                // 解释执行
+                // System.out.println("GS Engine Start!");
+                ByteCodeSerialize.serialize(bytecodeGenerator.getByteCode(), classDir + File.separator + "gclass" + File.separator + fileName + ".gclass");
+                if ("test".equals(fileName)) {
+                    Interpreter interpreter = new Interpreter();
+                    interpreter.eval(bytecodeGenerator.getByteCode());
+                }
+                // System.out.println("GS Engine End!");
             }
-            // System.out.println("GS Engine End!");
         }
     }
 }
