604a83bbde49b38fec69c3088a159e452274132b
957398123 <957398123@qq.com>
2026-03-25 09:55:41 +0800

    修改：修改NaN的字符连接操作。

--- src/main/java/org/gscript/vm/value/GSNaN.java
+++ src/main/java/org/gscript/vm/value/GSNaN.java
@@ -7,6 +7,11 @@ public class GSNaN extends GSObject {
 
     public static final GSNaN NAN = new GSNaN();
 
+    @Override
+    public String toStringValue() {
+        return "NaN";
+    }
+
     private GSNaN() {
         this.type = 10;
     }
